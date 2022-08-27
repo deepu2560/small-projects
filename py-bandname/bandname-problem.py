2d3d165cd9d56ddbe2f3af5fbe937b19224b5367
unknown <deepanshugulia256@gmail.com>
2022-08-27 18:23:14 +0530

    my first python project

---
+++ py-bandname/bandname-problem.py
@@ -0,0 +1,17 @@
+#1. Create a greeting for your program.
+
+print("Hello! there")
+print("Welcome to band generator")
+
+#2. Ask the user for the city that they grew up in.
+
+cityName = input("Please enter your city name. \n")
+
+#3. Ask the user for the name of a pet.
+
+petName = input("Please enter your pet name. \n")
+
+#4. Combine the name of their city and pet and show them their band name.
+
+final_bandName = cityName + petName
+print("Your band name can be :- "+final_bandName)
