6cdc1b5a99c4916217165c4973e52492e467bda0
unknown <deepanshugulia256@gmail.com>
2022-08-31 17:05:02 +0530

    learning python

---
+++ py-rollercoaster/rollercoaster.py
@@ -0,0 +1,7 @@
+print("Welcome to rollercoaster ride")
+height = int(input("What is your height ?\n"))
+
+if(height>=120):
+  print("You can ride rollercoaster")
+else:
+  print("Sorry! You have to grow more to ride rollercoaster")
