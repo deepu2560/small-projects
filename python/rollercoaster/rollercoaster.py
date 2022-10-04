50e1f4d6f59b6f6ad433417d7ef0c6960a52306a
unknown <deepanshugulia256@gmail.com>
2022-10-04 13:51:01 +0530

    added free rider for midlife crises in roller coaster

--- python/rollercoaster/rollercoaster.py
+++ python/rollercoaster/rollercoaster.py
@@ -8,6 +8,8 @@ if(height>=120):
     print("Please pay $5.")
   elif(age<=18):
     print("Please pay $7.")
+  elif (age > 45 and age < 55):
+    print("You can ride free of cost")
   else:
     print("Please pay $12.")
 else:
