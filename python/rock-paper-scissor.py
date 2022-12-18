e5297c5290aade633101dafc3f897c5adbcfd7e7
unknown <deepanshugulia256@gmail.com>
2022-12-18 14:40:18 +0530

    done with rock paper scissor game

--- python/rock-paper-scissor.py
+++ python/rock-paper-scissor.py
@@ -15,19 +15,22 @@ user_choice = int(
         "Your turn. Choose 0 for rock, 1 for paper and 2 for scissors. What's your choice? \n"
     ))
 
-computer_choice = random.randint(0, len(lists) - 1)
-
-print(f"You choose: {lists[user_choice]} \n")
-
-print(f"Computer choose: {lists[computer_choice]} \n")
-
-if (user_choice == 0 and computer_choice == 1):
-    print("You lose this Match. Try again...")
-elif (user_choice == 1 and computer_choice == 2):
-    print("You lose this Match. Try again...")
-elif (user_choice == 2 and computer_choice == 0):
-    print("You lose this Match. Try again...")
-elif (user_choice == computer_choice):
-    print("Match draw. Let's play another one...")
+if(user_choice>2 or user_choice<0):
+    print("Your chose invalid number try again...")
 else:
-    print("You won this Match. Yeppie...")
+    computer_choice = random.randint(0, len(lists) - 1)
+
+    print(f"You choose: {lists[user_choice]} \n")
+
+    print(f"Computer choose: {lists[computer_choice]} \n")
+
+    if (user_choice == 0 and computer_choice == 1):
+        print("You lose this Match. Try again...")
+    elif (user_choice == 1 and computer_choice == 2):
+        print("You lose this Match. Try again...")
+    elif (user_choice == 2 and computer_choice == 0):
+        print("You lose this Match. Try again...")
+    elif (user_choice == computer_choice):
+        print("Match draw. Let's play another one...")
+    else:
+        print("You won this Match. Yeppie...")
