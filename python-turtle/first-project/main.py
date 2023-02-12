d2c8bc5ed2abed92d496b66d3bdd973eaa5b23d9
unknown <deepanshugulia256@gmail.com>
2023-02-12 16:53:55 +0530

    learned some basic

--- python-turtle/first-project/main.py
+++ python-turtle/first-project/main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from turtle import Turtle, Screen
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+demo = Turtle()
+print(demo)
 
+demo.forward(100)
+demo.color("red")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+demo_screen = Screen()
+print(demo_screen.canvheight)
+demo_screen.exitonclick()
