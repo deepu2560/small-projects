a71d309fd38642fc3a19e114d3675d4a74c117d1
unknown <deepanshugulia256@gmail.com>
2023-02-24 01:42:00 +0530

    made a red heart with turtle graphic

---
+++ python-oops/colorful-turtle-art/main.py
@@ -0,0 +1,28 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+
+
+def curve(num):
+    for i in range(num):
+        timmy_the_turtle.left(1)
+        timmy_the_turtle.forward(1)
+
+
+timmy_the_turtle.shape("turtle")
+
+timmy_the_turtle.fillcolor("red")
+timmy_the_turtle.begin_fill()
+timmy_the_turtle.left(55)
+timmy_the_turtle.forward(200)
+curve(num=200)
+timmy_the_turtle.right(160)
+curve(num=200)
+timmy_the_turtle.left(5)
+timmy_the_turtle.forward(210)
+timmy_the_turtle.left(100)
+timmy_the_turtle.end_fill()
+
+
+screen = Screen()
+screen.exitonclick()
