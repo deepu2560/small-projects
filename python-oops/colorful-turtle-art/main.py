77203297b69437cffb3ad266ad3c5754c1bf27d7
unknown <deepanshugulia256@gmail.com>
2023-02-27 00:02:08 +0530

    done making a spiral with turtle graphics

--- python-oops/colorful-turtle-art/main.py
+++ python-oops/colorful-turtle-art/main.py
@@ -2,29 +2,14 @@ import turtle as t
 import random
 
 t.colormode(255)
+
 timmy_the_turtle = t.Turtle()
-timmy_the_turtle.speed("fast")
-timmy_the_turtle.pensize(5)
-
-
-def random_color():
-    red = random.randint(0, 255)
-    green = random.randint(0, 255)
-    blue = random.randint(0, 255)
-
-    color_tuple = (red, green, blue)
-
-    return color_tuple
-
-
-turn_angle = [0, 90, 180, 270]
+timmy_the_turtle.speed("fastest")
 
 for _ in range(200):
-    clr = random_color()
-    timmy_the_turtle.color(clr)
-    timmy_the_turtle.forward(20)
-    timmy_the_turtle.left(random.choice(turn_angle))
-
+    timmy_the_turtle.circle(50)
+    timmy_the_turtle.right(10)
+    timmy_the_turtle.forward(10)
 
 screen = t.Screen()
 screen.exitonclick()
