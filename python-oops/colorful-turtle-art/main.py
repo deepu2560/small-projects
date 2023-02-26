9a93314d96ed908425f39c38d4bbea9ecd37b931
unknown <deepanshugulia256@gmail.com>
2023-02-26 11:37:59 +0530

    made a squar with turtle graphic

--- python-oops/colorful-turtle-art/main.py
+++ python-oops/colorful-turtle-art/main.py
@@ -2,28 +2,18 @@ from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
 
-print("hello world")
-def curve(num):
-    for i in range(num):
-        timmy_the_turtle.left(1)
-        timmy_the_turtle.forward(1)
+
+def turn_left():
+    timmy_the_turtle.left(90)
+    timmy_the_turtle.forward(100)
 
 
-timmy_the_turtle.shape("turtle")
-
-timmy_the_turtle.fillcolor("red")
-timmy_the_turtle.begin_fill()
-timmy_the_turtle.left(55)
-timmy_the_turtle.forward(200)
-curve(num=200)
-timmy_the_turtle.right(160)
-curve(num=200)
-timmy_the_turtle.left(5)
-timmy_the_turtle.forward(210)
-timmy_the_turtle.left(100)
-timmy_the_turtle.end_fill()
-
+timmy_the_turtle.forward(100)
+turn_left()
+turn_left()
+turn_left()
 
 screen = Screen()
 screen.exitonclick()
+
 print("turtle graphic shape made")
