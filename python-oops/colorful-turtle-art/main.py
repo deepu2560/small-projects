d26a7fe9bee0ddec528ee2e22eb0966385ba8d8f
unknown <deepanshugulia256@gmail.com>
2023-02-26 13:49:50 +0530

    turtle random walk

--- python-oops/colorful-turtle-art/main.py
+++ python-oops/colorful-turtle-art/main.py
@@ -1,12 +1,25 @@
 from turtle import Turtle, Screen
+import random
 
 timmy_the_turtle = Turtle()
+timmy_the_turtle.speed(2)
+timmy_the_turtle.pensize(5)
+
+colors = ["black", "teal", "slate gray", "aquamarine", "cornflower blue", "royal blue",
+          "blue", "navy", "midnight blue", "deep sky blue", "dodger blue", "steel blue", "cyan", "medium turquoise",
+          "green", "dark green", "green yellow", "orange", "peru", "goldenrod", "brown", "orange red", "red",
+          "deep pink", "purple", "magenta", "dark violet", "indigo", "lavender", "dark slate blue", "yellow"]
+
+for _ in range(100):
+    timmy_the_turtle.color(random.choice(colors))
+    timmy_the_turtle.forward(20)
+    turn = random.randint(1, 2)
+    if turn == 1:
+        timmy_the_turtle.left(90)
+    else:
+        timmy_the_turtle.right(90)
 
 
-for num in range(3, 10):
-    for _ in range(num):
-        timmy_the_turtle.forward(100)
-        timmy_the_turtle.left(360/num)
-
-
+screen = Screen()
+screen.exitonclick()
 print("turtle graphic shape made")
