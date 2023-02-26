0f0cc03318fcbaa7fae569bab265690cd0bc50b7
unknown <deepanshugulia256@gmail.com>
2023-02-26 14:22:51 +0530

    creating random rgb color with tuple

--- python-oops/colorful-turtle-art/main.py
+++ python-oops/colorful-turtle-art/main.py
@@ -1,24 +1,31 @@
-from turtle import Turtle, Screen
+import turtle as t
 import random
 
-timmy_the_turtle = Turtle()
+t.colormode(255)
+timmy_the_turtle = t.Turtle()
 timmy_the_turtle.speed("fast")
 timmy_the_turtle.pensize(5)
 
-colors = ["black", "teal", "slate gray", "aquamarine", "cornflower blue", "royal blue",
-          "blue", "navy", "midnight blue", "deep sky blue", "dodger blue", "steel blue", "cyan", "medium turquoise",
-          "green", "dark green", "green yellow", "orange", "peru", "goldenrod", "brown", "orange red", "red",
-          "deep pink", "purple", "magenta", "dark violet", "indigo", "lavender", "dark slate blue", "yellow"]
+
+def random_color():
+    red = random.randint(0, 255)
+    green = random.randint(0, 255)
+    blue = random.randint(0, 255)
+
+    color_tuple = (red, green, blue)
+
+    return color_tuple
+
 
 turn_angle = [0, 90, 180, 270]
 
 for _ in range(200):
-    clr = float(f"0.{random.randint(0, 255)}"), float(f"0.{random.randint(0, 255)}"), float(f"0.{random.randint(0, 255)}")
+    clr = random_color()
     timmy_the_turtle.color(clr)
     timmy_the_turtle.forward(20)
     timmy_the_turtle.left(random.choice(turn_angle))
 
 
-screen = Screen()
+screen = t.Screen()
 screen.exitonclick()
 print("turtle graphic shape made")
