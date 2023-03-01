1c006230047df4447221660f807aa81f6c3ad50a
unknown <deepanshugulia256@gmail.com>
2023-03-02 01:05:55 +0530

    done making hirst painting and extracted color from other image

--- python-oops/hirst-painting/main.py
+++ python-oops/hirst-painting/main.py
@@ -1,7 +1,7 @@
 import colorgram
 import turtle as t
 
-colors = colorgram.extract("image.jpg", 35)
+colors = colorgram.extract("image.jpg", 30)
 print(len(colors))
 
 draw = t.Turtle()
@@ -9,22 +9,17 @@ t.colormode(255)
 
 count = -1
 
-# for num in range(1, 6):
-#     draw.penup()
-#     for _ in range(7):
-#         print(count)
-#         count += 1
-#         rgb = colors[count].rgb
-#         clr = (rgb.r, rgb.g, rgb.b)
-#         draw.pendown()
-#         draw.dot(20, clr)
-#         draw.forward(50)
-#         draw.penup()
-#     draw.penup()
-#     draw.home()
-#     draw.left(90)
-#     draw.forward(10*num)
-#     draw.right(90)
+for num in range(0, 5):
+    draw.penup()
+    draw.goto(-50.00, float(50*num))
+    for _ in range(6):
+        count += 1
+        rgb = colors[count].rgb
+        clr = (rgb.r, rgb.g, rgb.b)
+        draw.pendown()
+        draw.dot(20, clr)
+        draw.penup()
+        draw.forward(50)
 
 screen = t.Screen()
 screen.exitonclick()
