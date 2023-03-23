2dc75f920aeccaf5d066b0776f61c81648abd8d5
unknown <deepanshugulia256@gmail.com>
2023-03-23 22:10:08 +0530

    created a sketch board

--- python-oops/sketch_art/main.py
+++ python-oops/sketch_art/main.py
@@ -1,14 +1,41 @@
 import turtle as trtl
 
 myturtle = trtl.Turtle()
+myturtle.shape("circle")
+myturtle.pensize(5)
 
 
 def move_forward():
-    myturtle.forward(100)
+    myturtle.forward(10)
+
+
+def move_backend():
+    myturtle.backward(10)
+
+
+def move_left():
+    new_head = myturtle.heading() + 10
+    myturtle.setheading(new_head)
+
+
+def move_right():
+    new_head = myturtle.heading() - 10
+    myturtle.setheading(new_head)
+
+
+def reset():
+    myturtle.penup()
+    myturtle.clear()
+    myturtle.home()
+    myturtle.pendown()
 
 
 scrn = trtl.Screen()
 scrn.listen()
 scrn.onkey(move_forward, "w")
+scrn.onkey(move_backend, "s")
+scrn.onkey(move_left, "a")
+scrn.onkey(move_right, "d")
+scrn.onkey(reset, "e")
 scrn.exitonclick()
 
