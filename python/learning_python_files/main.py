b3487db756ac7f7974ebe5eb6d77a1a60625776e
unknown <deepanshugulia256@gmail.com>
2023-03-29 18:58:51 +0530

    learned about read and write of files

--- python/learning_python_files/main.py
+++ python/learning_python_files/main.py
@@ -2,3 +2,8 @@ with open("demo.txt") as file:
     content = file.read()
     print(content)
 
+with open("demo.txt", mode="a") as file:
+    file.write("\nNew Text file")
+
+with open("new_demo.txt", mode="a") as file:
+    file.write("Hello world")
