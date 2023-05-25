e42f76ddc5229041d580f81633d150e726071a22
unknown <deepanshugulia256@gmail.com>
2023-05-25 20:50:19 +0530

    create custom dataset and it's csv file

--- python-oops/weathercsv/main.py
+++ python-oops/weathercsv/main.py
@@ -17,19 +17,19 @@ import pandas
 weatherData = pandas.read_csv("weather_data.csv")
 # print(weatherData["temp"])
 
-data_dict = weatherData.to_dict()
+# data_dict = weatherData.to_dict()
 # print(data_dict)
 
-temp_list = weatherData["temp"].to_list()
+# temp_list = weatherData["temp"].to_list()
 # print(temp_list)
 
-average_weather = sum(temp_list)/len(temp_list)
+# average_weather = sum(temp_list)/len(temp_list)
 # print(round(average_weather))
 
-mean_weather = weatherData["temp"].mean()
+# mean_weather = weatherData["temp"].mean()
 # print(mean_weather)
 
-max_weather = weatherData["temp"].max()
+# max_weather = weatherData["temp"].max()
 # print(max_weather)
 
 # print(weatherData["condition"])
@@ -39,3 +39,14 @@ max_weather = weatherData["temp"].max()
 # print(weatherData[weatherData.day == "Monday"])
 # print(weatherData[weatherData.temp == weatherData["temp"].max()])
 
+# monday_data = weatherData[weatherData.day == "Monday"]
+# print((monday_data.temp * (9/5))+32)
+
+# creating a csv file or students data
+demo_data = {
+    "name": ["deepanshu", "soni", "tushar", "moni", "komal"],
+    "age": [22, 19, 19, 25, 22],
+}
+
+student_data = pandas.DataFrame(demo_data)
+student_data.to_csv("./student_data.csv")
