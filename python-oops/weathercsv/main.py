db4878d73caf2f41de790932a94c4460c863319e
unknown <deepanshugulia256@gmail.com>
2023-05-25 20:34:28 +0530

    learned about getting columns and rows and some built-in functions

--- python-oops/weathercsv/main.py
+++ python-oops/weathercsv/main.py
@@ -19,4 +19,23 @@ weatherData = pandas.read_csv("weather_data.csv")
 
 data_dict = weatherData.to_dict()
 # print(data_dict)
-print(data_dict["temp"].to_list())
+
+temp_list = weatherData["temp"].to_list()
+# print(temp_list)
+
+average_weather = sum(temp_list)/len(temp_list)
+# print(round(average_weather))
+
+mean_weather = weatherData["temp"].mean()
+# print(mean_weather)
+
+max_weather = weatherData["temp"].max()
+# print(max_weather)
+
+# print(weatherData["condition"])
+# print(weatherData.condition)
+
+# getting rows
+# print(weatherData[weatherData.day == "Monday"])
+# print(weatherData[weatherData.temp == weatherData["temp"].max()])
+
